f52a123f893f351e22e4f46fa86657944f1165fb
David Alvarez <david.a@casautomotive.com>
2025-04-03 10:03:18 -0400

    contact form completed

--- website/core/forms.py
+++ website/core/forms.py
@@ -11,6 +11,22 @@ class BaseForm(forms.Form):
                 field.widget.attrs.update({
                     'class': 'block w-full rounded-lg border border-gray-200 px-5 py-3 leading-6 placeholder-gray-500 focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-primary'
                 })
+            elif isinstance(field.widget, forms.TextInput):
+                field.widget.attrs.update({
+                    'class': 'block w-full rounded-lg border border-gray-200 px-5 py-3 leading-6 placeholder-gray-500 focus:border-primary-500 focus:ring focus:ring-primary-500/50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-primary-500'
+                })
+            elif isinstance(field.widget, forms.EmailInput):
+                field.widget.attrs.update({
+                    'class': 'block w-full rounded-lg border border-gray-200 px-5 py-3 leading-6 placeholder-gray-500 focus:border-primary-500 focus:ring focus:ring-primary-500/50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-primary-500'
+                })
+            elif isinstance(field.widget, forms.PasswordInput):
+                field.widget.attrs.update({
+                    'class': 'block w-full rounded-lg border border-gray-200 px-5 py-3 leading-6 placeholder-gray-500 focus:border-primary-500 focus:ring focus:ring-primary-500/50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-primary-500'
+                })
+            elif isinstance(field.widget, forms.Textarea):
+                field.widget.attrs.update({
+                    'class': 'block w-full rounded-lg border border-gray-200 px-5 py-3 leading-6 placeholder-gray-500 focus:border-primary-500 focus:ring focus:ring-primary-500/50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-primary-500'
+                })
             elif isinstance(field.widget, forms.Select):
                 field.widget.attrs.update({
                     'class': 'block w-full rounded-lg border border-gray-200 px-5 py-3 leading-6 focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:focus:border-primary'
@@ -19,8 +35,21 @@ class BaseForm(forms.Form):
                 field.label = f'<label class="font-medium">{field.label}</label>'
 
 class LoginForm(BaseForm):
-    username = forms.CharField(max_length=150, required=True)
-    password = forms.CharField(widget=forms.PasswordInput, required=True)
+    username = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Username...',
+            'required': True
+        }),
+        required=True
+    )
+    password = forms.CharField(
+        max_length=100,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password...',
+            'required': True
+        }),
+        required=True)
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
@@ -30,10 +59,38 @@ class LoginForm(BaseForm):
             raise ValidationError("Username does not exist.")
         return username
 
-    def clean_password(self):
-        password = self.cleaned_data.get('password')
-        try:
-            user = User.objects.get(password=password)
-        except User.DoesNotExist:
-            raise ValidationError("Password does not exist.")
-        return password
+class ContactForm(forms.Form):
+    first_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'First Name',
+            'required': True
+        }),
+        required=True
+    )
+    
+    last_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Last Name',
+            'required': True
+        }),
+        required=True
+    )
+    
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Email',
+            'required': True
+        }),
+        required=True
+    )
+    
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Your Message...',
+            'rows': 6,
+            'required': True
+        }),
+        required=True
+    )
