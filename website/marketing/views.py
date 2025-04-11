7fe11035b5b97563a4539ca7f0ca7e9481e19fe3
David Alvarez <david.a@casautomotive.com>
2025-04-11 15:01:49 -0400

    default views

--- website/marketing/views.py
+++ website/marketing/views.py
@@ -1,8 +1,6 @@
 from .models import ConversionLog, CallTrackingNumber
-from .forms import ConversionLogFilterForm, CallTrackingFilterForm
+from .forms import ConversionLogFilterForm, CallTrackingFilterForm, CallTrackingNumberForm
 from website.crm.views import CRMBaseListView, CRMBaseDetailView, CRMBaseDeleteView, CRMBaseCreateView, CRMBaseUpdateView
-from django.http import HttpResponseRedirect
-from django.urls import reverse_lazy
 
 class ConversionLogListView(CRMBaseListView):
     template_name = "log_list.html"
@@ -11,30 +9,17 @@ class ConversionLogListView(CRMBaseListView):
     filter_form_class = ConversionLogFilterForm
 
 class CallTrackingNumberListView(CRMBaseListView):
-    template_name = "call_tracking_number_list.html"
     model = CallTrackingNumber
-    context_object_name = "numbers"
     filter_form_class = CallTrackingFilterForm
 
 class CallTrackingNumberCreateView(CRMBaseCreateView):
-    model = CallTrackingNumber
-    fields = ['platform_id', 'call_tracking_number', 'campaign']
-    template_name = 'call_tracking_number_create.html'
+    form_class = CallTrackingNumberForm
 
 class CallTrackingNumberUpdateView(CRMBaseUpdateView):
+    form_class = CallTrackingNumberForm
+
+class CallTrackingNumberDetailView(CRMBaseDetailView):
     model = CallTrackingNumber
-    fields = ['platform_id', 'call_tracking_number', 'campaign']
-    template_name = 'call_tracking_number_update.html'
 
 class CallTrackingNumberDeleteView(CRMBaseDeleteView):
     model = CallTrackingNumber
-
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        self.object.delete()
-        return HttpResponseRedirect(self.success_url)
-
-class CallTrackingNumberDetailView(CRMBaseDetailView):
-    template_name = "call_tracking_number_detail.html"
-    model = CallTrackingNumber
-    context_object_name = "number"
