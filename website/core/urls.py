ae38e6526cc702e25b3988ca10ba08f3d307d33d
David Alvarez <david.a@casautomotive.com>
2025-04-14 15:39:05 -0400

    hero section

--- website/core/urls.py
+++ website/core/urls.py
@@ -8,12 +8,6 @@ urlpatterns = [
     path('privacy-policy', views.PrivacyPolicyView.as_view(), name='privacy_policy'),
     path('terms-and-conditions', views.TermsAndConditionsView.as_view(), name='terms_and_conditions'),
     path('robots.txt', views.RobotsTxtView.as_view(), name='robots_txt'),
-    path('planning', views.PlanningLPView.as_view(), name='planning_lp'),
-    path('staffing', views.StaffingLPView.as_view(), name='staffing_lp'),
     path('quote', views.QuoteView.as_view(), name='quote'),
-    path('logout', views.LogoutView.as_view(), name='logout'),
-    
-    # Partial templates
-    path('pop-up-modal', views.get_pop_up_modal, name='pop_up_modal'),
-    path('error-modal', views.get_error_modal, name='error_modal')
+    path('logout', views.LogoutView.as_view(), name='logout')
 ]
