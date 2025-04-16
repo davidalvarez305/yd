3ffa3cba9e13ccb129904f7552d5e5dc9f8b6783
unknown <david@southfloridaathleticclub.com>
2025-04-15 21:31:14 -0400

    visit update view

--- website/marketing/views.py
+++ website/marketing/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from .models import ConversionLog, CallTrackingNumber, Visit
 from .forms import ConversionLogFilterForm, CallTrackingFilterForm, CallTrackingNumberForm, VisitForm, VisitFilterForm
 from crm.views import CRMBaseListView, CRMBaseDetailView, CRMBaseDeleteView, CRMBaseCreateView, CRMBaseUpdateView
+from django.views.generic.edit import UpdateView
 
 class ConversionLogListView(CRMBaseListView):
     template_name = "log_list.html"
@@ -28,5 +30,11 @@ class VisitListView(CRMBaseListView):
     model = Visit
     filter_form_class = VisitFilterForm
 
-class VisitUpdateView(CRMBaseUpdateView):
+class VisitUpdateView(UpdateView):
     form_class = VisitForm
+    model = Visit
+    http_method_names = ['post']
+    
+    def form_valid(self, form):
+        form.save()
+        return HttpResponse("Updated")
