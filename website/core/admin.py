d65f9a211efa3e9ab22c6f9841c093479c885336
unknown <david@southfloridaathleticclub.com>
2025-04-18 15:35:10 -0400

    finito

--- website/core/admin.py
+++ website/core/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import LeadStatus
+from .models import LeadStatus, LeadInterest, NextAction, ServiceType, UnitType, InvoiceType
 
 admin.site.register(LeadStatus)
+admin.site.register(LeadInterest)
+admin.site.register(NextAction)
+admin.site.register(ServiceType)
+admin.site.register(UnitType)
+admin.site.register(InvoiceType)
