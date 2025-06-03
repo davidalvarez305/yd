78317e197fa9c0b46ad47517eb9fffc99c406a75
unknown <david@southfloridaathleticclub.com>
2025-06-03 02:31:26 -0400

    aa

--- website/crm/views.py
+++ website/crm/views.py
@@ -769,7 +769,7 @@ class QuoteDetailView(CRMDetailTemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['quote_service_table'] = QuoteServiceTable(data=self.object.quote_services.all())
+        context['quote_service_table'] = QuoteServiceTable(data=self.object.quote_services.all(), request=self.request)
         context['quote_service_form'] = QuoteServiceForm(initial={
             'quote': self.object
         })
