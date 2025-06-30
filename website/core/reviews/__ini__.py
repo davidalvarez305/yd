1fdc4f4158f751cfc888622f53ca89db9f446c49
David Alvarez <david.a@casautomotive.com>
2025-06-30 13:29:46 -0400

    sync reviews

--- website/core/reviews/__ini__.py
+++ website/core/reviews/__ini__.py
@@ -7,6 +7,10 @@ class ReviewsService(LazyObject):
     def _setup(self):
         cls = import_string(settings.REVIEWS_SERVICE)
 
-        self._wrapped = cls()
+        self._wrapped = cls(
+            access_token=settings.GOOGLE_API_ACCESS_TOKEN,
+            account_id=settings.GOOGLE_BUSINESS_PROFILE_ACCOUNT_ID,
+            location_id=settings.GOOGLE_BUSINESS_PROFILE_LOCATION_ID,
+        )
 
 reviews_service = ReviewsService()
