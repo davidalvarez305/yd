59e10c10e660830bc6c1a946a049ac12c5bd2901
David Alvarez <david.a@casautomotive.com>
2025-07-10 14:48:16 -0400

    media url

--- website/core/widgets.py
+++ website/core/widgets.py
@@ -157,7 +157,7 @@ class AudioWidget(TableCellWidget):
     def render(self, row=None, request=None):
         object_key = self.data.get('value')
         if object_key:
-            media_path = getattr(settings, 'AWS_S3_CUSTOM_DOMAIN', settings.MEDIA_key)
+            media_path = getattr(settings, 'AWS_S3_CUSTOM_DOMAIN', settings.MEDIA_URL)
             src = media_path + object_key
 
             return f"""
