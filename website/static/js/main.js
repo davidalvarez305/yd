2dbbc2dbcf53b7615112f427a1744a55cd8a91ce
David Alvarez <david.a@casautomotive.com>
2025-07-11 16:19:02 -0400

    ok

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -16,3 +16,20 @@ function preserveQuerystring() {
 }
 
 document.addEventListener("DOMContentLoaded", () => preserveQuerystring());
+
+// HTMX
+function handleSubmissionWorkflow(event, isLoading) {
+	const form = event.target;
+	const spinnerSelector = form.getAttribute('data-loading-target');
+	const spinner = document.querySelector(spinnerSelector);
+	const submitButton = form.querySelector('button[type="submit"]');
+
+	if (!spinnerSelector || !spinner || !submitButton) return;
+
+	isLoading ? spinner.classList.remove('hidden') : spinner.classList.add('hidden');
+	isLoading ? spinner.classList.add('animate-spin') : spinner.classList.remove('animate-spin');
+	isLoading ? submitButton.disabled = true : submitButton.disabled = false;
+};
+
+document.addEventListener('htmx:configRequest', event => handleSubmissionWorkflow(event, true));
+document.addEventListener('htmx:afterRequest', event => handleSubmissionWorkflow(event, false));
