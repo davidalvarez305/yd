695e747e0d49350c6988be4ab8be641f1f8ba3db
David Alvarez <david.a@casautomotive.com>
2025-07-17 13:29:15 -0400

    abs

--- website/core/email/gmail.py
+++ website/core/email/gmail.py
@@ -1,39 +1,14 @@
-import base64
-import os
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from email.mime.text import MIMEText
-
-from website import settings
 from .base import EmailService
+from core.google.api import google_api_service
 
 class GmailService(EmailService):
     def __init__(self):
         """Inject a Gmail API client."""
-        self.client = initialize_gmail_client()
+        self.client = google_api_service()
 
     def send_email(self, to: str, subject: str, body: str) -> None:
         """Send an email using Gmail API."""
-        message = MIMEText(body)
-        message["to"] = to
-        message["subject"] = subject
-        raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-
-        self.client.users().messages().send(userId="me", body={"raw": raw_message}).execute()
-
-
-def initialize_gmail_client():
-    """Authenticate and return a Gmail API client."""
-    creds = None
-
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", settings.GOOGLE_API_SCOPES)
-
-    if not creds or not creds.valid:
-        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", settings.GOOGLE_API_SCOPES)
-        creds = flow.run_local_server(port=0)
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-
-    return build("gmail", "v1", credentials=creds)
+        try:
+            self.client.send_email(to=to, subject=subject, body=body)
+        except Exception as e:
+            raise Exception(f'Error sending e-mail.: {e}')
--- website/core/google/api/base.py
+++ website/core/google/api/base.py
@@ -2,9 +2,17 @@ from abc import ABC, abstractmethod
 
 class GoogleAPIServiceInterface(ABC):
     @abstractmethod
-    def get_sheets_data(self, lead):
+    def get_sheet_data(self, spreadsheet_id: str, range_name: str) -> list[dict]:
+        pass
+
+    @abstractmethod
+    def append_sheet_data(self, spreadsheet_id: str, range_name: str, data: list[dict]) -> dict:
         pass
 
     @abstractmethod
     def sync_reviews(self):
+        pass
+
+    @abstractmethod
+    def send_email(self, to: str, subject: str, body: str) -> None:
         pass
