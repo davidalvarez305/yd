ee3478ba6ac90dfcdb0d95fd65cdc6beb1ea390f
David Alvarez <david.a@casautomotive.com>
2025-07-21 14:34:42 -0400

    lead marketing

--- website/core/admin.py
+++ website/core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from core.models import CallTracking, CallTrackingNumber, EventRole, FacebookAccessToken, GoogleAccessToken, IngredientCategory, Invoice, Lead, LeadStatus, LeadInterest, AdCampaign, LeadStatusHistory, Message, NextAction, PhoneCall, PhoneCallStatusHistory, ServiceType, Store, Unit, UnitConversion, UnitType, InvoiceType, User, GoogleReview, Visit
+from core.models import CallTracking, CallTrackingNumber, EventRole, FacebookAccessToken, GoogleAccessToken, IngredientCategory, Invoice, Lead, LeadMarketing, LeadStatus, LeadInterest, AdCampaign, LeadStatusHistory, Message, NextAction, PhoneCall, PhoneCallStatusHistory, ServiceType, Store, Unit, UnitConversion, UnitType, InvoiceType, User, GoogleReview, Visit
 
 admin.site.register(CallTracking)
 admin.site.register(CallTrackingNumber)
 
 admin.site.register(Lead)
+admin.site.register(LeadMarketing)
 admin.site.register(LeadStatus)
 admin.site.register(LeadInterest)
 admin.site.register(NextAction)
