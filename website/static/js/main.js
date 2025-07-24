4da7e2748bf3c1cb2714613e36cb926062e15f1f
David Alvarez <david.a@casautomotive.com>
2025-07-24 11:03:58 -0400

    nav

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,23 +1,3 @@
-function preserveQuerystring() {
-	if (["crm"].some(link => window.location.pathname.includes(link))) return;
-
-	const links = document.querySelectorAll('a');
-
-	links.forEach(link => {
-		link.addEventListener('click', function () {
-			const qs = window.location.search;
-			if (qs.length > 0) {
-				const url = new URL(this.href, window.location.origin);
-				url.search = url.search ? `${url.search}&${qs.substring(1)}` : qs;
-				this.href = url.toString();
-			}
-		});
-	});
-}
-
-document.addEventListener("DOMContentLoaded", () => preserveQuerystring());
-
-// HTMX
 function handleSubmissionWorkflow(event, isLoading) {
 	const form = event.target;
 	const spinnerSelector = form.getAttribute('data-loading-target');
