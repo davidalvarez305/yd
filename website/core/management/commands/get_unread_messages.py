c07e195f3cd2045a7a44aa8bd476c27db9cca6f1
David Alvarez <david.a@casautomotive.com>
2025-07-29 10:26:03 -0400

    handle

--- website/core/management/commands/get_unread_messages.py
+++ website/core/management/commands/get_unread_messages.py
@@ -7,8 +7,6 @@ from core.email import email_service
 from website import settings
 
 class Command(BaseCommand):
-    help = 'Fetch and print Facebook lead data for a given leadgen_id using the existing facebook_api_service instance.'
-
     def handle(self, *args, **options):
         try:
             unread_messages = Message.objects.filter(is_read=False).count()
