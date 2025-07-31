8a7ff901ac44c85e065925aec7181c37cb301103
David Alvarez <david.a@casautomotive.com>
2025-07-31 14:26:14 -0400

    now

--- website/marketing/signals.py
+++ website/marketing/signals.py
@@ -17,11 +17,9 @@ def create_data_dict(lead: Lead, event_name=None, event=None):
         'event_name': event_name,
         'ip_address': lead.lead_marketing.ip,
         'user_agent': lead.lead_marketing.user_agent,
-        'event_time': int(now().timestamp() * 1000),
+        'event_time': now().timestamp() * 1000,
         'phone_number': lead.phone_number,
     }
-
-    print('data: ', data)
 
     if event_name == 'event_booked' and event:
         data.update({
