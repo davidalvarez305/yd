4465926dd80a512c6a7b7358bff4e16696c6623c
David Alvarez <david.a@casautomotive.com>
2025-11-12 14:12:12 -0500

    save payment intent

--- website/core/billing/stripe.py
+++ website/core/billing/stripe.py
@@ -174,7 +174,7 @@ class StripeBillingService(BillingServiceInterface):
                 mode='payment',
                 ui_mode='hosted',
                 customer_creation="always",
-                saved_payment_method_options={"payment_method_save": "enabled"},
+                payment_intent_data={ 'setup_future_usage': 'off_session' },
                 success_url=settings.ROOT_DOMAIN + reverse('success_payment', kwargs={'external_id': str(invoice.external_id)}),
                 cancel_url=settings.ROOT_DOMAIN + reverse('cancel_payment', kwargs={'external_id': str(invoice.external_id)}),
                 metadata={
